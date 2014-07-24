c1eb6e4c8e9ff3176d57a085ece69b95a70313d4
Grooblesign <p.j.garner@gmail.com>
2014-07-24 15:57:59 +0100

    error handling

--- public/angular/controllers/maincontroller.js
+++ public/angular/controllers/maincontroller.js
@@ -5,11 +5,15 @@
     var MainController = function($scope, $http) {
         $scope.message = "BPL Predictions";
 		
+        var onError = function(reason) {
+			$scope.error = reason;
+		}
+
 		var onMatches = function(response) {
 			$scope.matches = response.data;
 		};
   
-		$http.get("/matches").then(onMatches);
+		$http.get("/matche").then(onMatches, onError);
 		
 		$scope.editMatch = function(match) {
 			confirm("Edit this match?");
